f71e9d0149c6b7ad9b5fa5c685bb27771201d462
Carlos Sanchez <randomouscrap98@aol.com>
2023-12-30 04:30:07 -0500

    Split login function

--- main.c
+++ main.c
@@ -20,6 +20,40 @@
 #include "screen.h"
 
 
+// Ask user for new login info. Writes login info to token file.
+void newlogin(struct LowcapiConfig * config)
+{
+   const int inputlength = 100;
+   char username[inputlength];
+   char password[inputlength];
+
+   while(1) {
+      printw("Username: ");
+      lc_getinput_simple(username, inputlength);
+      printw("\nPassword: ");
+      lc_getpass_simple(password, inputlength);
+      printw("\nLogging in...\n");
+      refresh();
+      char * output = NULL;
+      if(lc_consumeresponse(lc_login(username, password, config, 0), &output))
+      {
+         print_color(LCSCL_OK, "Token received, writing to file\n");
+         lc_storetoken(config, output);
+         break;
+      }
+      else if(output)
+      {
+         print_color(LCSCL_WARN, "Error: %s\n", output);
+      }
+      else
+      {
+         print_color(LCSCL_WARN, "Error: UNKNOWN\n");
+      }
+      free(output); //Apparently it's safe to call free on null
+      refresh();
+   }
+}
+
 int main(int argc, char * argv[])
 {
    struct LowcapiConfig config = lc_read_config(argc > 1 ? argv[1] : NULL);
@@ -41,40 +75,17 @@ int main(int argc, char * argv[])
    lc_freeresponse(response);
    refresh();
 
-
    char * token = lc_gettoken(&config);
-   const int inputlength = 100;
-   char username[inputlength];
-   char password[inputlength];
 
-   if(!token) {
+   while(!token)
+   {
       print_color(LCSCL_WARN, "No token file found, please login\n");
       refresh();
-      while(!token) {
-         printw("Username: ");
-         lc_getinput_simple(username, inputlength);
-         printw("\nPassword: ");
-         lc_getpass_simple(password, inputlength);
-         printw("\nLogging in...\n");
-         refresh();
-         char * output = NULL;
-         if(lc_consumeresponse(lc_login(username, password, &config, 0), &output))
-         {
-            token = output;
-            printw("Token: %s\n", token);
-         }
-         else if(output)
-         {
-            print_color(LCSCL_WARN, "Error: %s\n", output);
-            free(output);
-         }
-         else
-         {
-            print_color(LCSCL_WARN, "Error: UNKNOWN\n");
-         }
-         refresh();
-      }
+      newlogin(&config);
+      token = lc_gettoken(&config);
    }
+
+   printw("Token file found, testing login...\n");
 
    log_info("Program end");
    printw("Program end\n");
@@ -84,4 +95,5 @@ int main(int argc, char * argv[])
 
    return 0;
 }
+
 
