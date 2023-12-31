a3b349b85e6f2dc4a1719bfadce8ad4c5aacffeb
Carlos Sanchez <randomouscrap98@aol.com>
2023-12-31 00:12:51 -0500

    Starting csv testing

--- Makefile
+++ Makefile
@@ -37,13 +37,19 @@ endif
 # chatgpt told me that .PHONY is there to ensure the command always runs, even
 # if there's a file named as such. But I think it got it wrong when it didn't 
 # include 'all' in the list, see https://www.gnu.org/software/make/manual/html_node/Phony-Targets.html
-.PHONY: clean all run windows
+.PHONY: clean all run runtestcsv windows
 
 # First target always run, so it's "all" of course
 all: $(PRG)
 
 run: all
 	./$(PRG) 2> err.txt
+
+runtestcsv: testcsv
+	./$^ "csvtest/files"
+
+testcsv: mycsv.o csvtest/test.o
+	$(CC) $(CFLAGS) -o $@ $^
 
 # Main compilation, should produce 'lowcapi'
 $(PRG): $(OBJS)
@@ -54,4 +60,4 @@ $(PRG): $(OBJS)
 	$(CC) $(CFLAGS) -c -o $@ $<
 
 clean:
-	rm -f $(PRG) $(OBJS)
+	rm -f $(PRG) $(OBJS) testcsv
---
+++ csvtest/test.c
@@ -0,0 +1,72 @@
+#include "../mycsv.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char * basepath;
+
+char * loadfile(const char * filename)
+{
+   char path[512];
+   sprintf(path, "%s/%s", basepath, filename);
+
+   //Chatgpt
+   FILE * file = fopen(path, "r");  
+
+   if (file == NULL) {
+      perror("Error opening file");
+      exit(1);
+   }
+
+   // Determine the file size
+   fseek(file, 0, SEEK_END);
+   long file_size = ftell(file);
+   rewind(file);
+
+   // Allocate memory to store the entire file
+   char *buffer = (char *)malloc(file_size);
+   if (buffer == NULL) {
+      perror("Memory allocation failed");
+      fclose(file);
+      exit(1);
+   }
+
+   // Read the entire file into memory
+   size_t read_size = fread(buffer, 1, file_size, file);
+
+   if (read_size != (size_t)file_size) {
+      perror("Error reading file");
+      free(buffer);
+      fclose(file);
+      exit(1);
+   }
+
+   fclose(file);
+
+   return buffer;
+}
+
+struct CsvAnalysis analyze(char * filecontents)
+{
+   return csv_analyze(filecontents, filecontents + strlen(filecontents) - 1);
+}
+
+int main(int argc, char * argv[])
+{
+   if(argc > 1)
+   {
+      basepath = argv[1];
+   }
+   else
+   {
+      fprintf(stderr, "Must provide the path to the test files\n");
+      exit(1);
+   }
+
+   //The first arg is where the files are
+   char * simple1 = loadfile("simple1.csv");
+   struct CsvAnalysis analysis = analyze(simple1);
+
+   printf("Yeah: %d\n", analysis.lines);
+}
