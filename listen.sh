b99ef7e6bf256dded91718a6c8c15dc4ef3c63a6
Carlos Sanchez <randomouscrap98@aol.com>
2024-02-17 17:07:01 -0500

    Listen more robust + working

--- listen.sh
+++ listen.sh
@@ -3,20 +3,17 @@
 # A sample listener script, which will continuously read
 # messages from the requested room (singular)
 
-# Check if URL environment variable is set
 if [ -z "$CAPI_URL" ]; then
     >&2 echo "Error: CAPI_URL environment variable is not set."
     exit 1
 fi
 
-# Check if token is set (it must be)
 if [ -z "$CAPI_TOKEN" ]; then
     >&2 echo "Error: CAPI_TOKEN environment variable is not set."
     exit 1
 fi
 
-if [ "$#" -lt 1 ] 
-then
+if [ "$#" -lt 1 ]; then
     echo "Error: you must include at least the room number!"
     exit 1
 fi
@@ -24,15 +21,14 @@ fi
 room=$1
 mid=-1
 
-if [ -z "$2" ]
-then
+if [ -z "$2" ]; then
    pull="30"
 else
    pull="$2"
 fi
 
 # Check who the user is
-username=$(curl -s -H "Authorization: Bearer $CAPI_TOKEN" -G \
+export capi_username=$(curl -s -H "Authorization: Bearer $CAPI_TOKEN" -G \
    "$CAPI_URL/small/me" | python3 -c '
 import csv
 import sys
@@ -41,12 +37,15 @@ for row in reader:
    print(row[1])
 ')
 
-echo "Listening as $username"
+echo "Listening as $capi_username"
 
 # Prints the normal messages to stdout and prints the max mid to stderr
 pymessage='
 import csv
 import sys
+import textwrap
+import shutil
+import os
 try:
    from colorama import init, Fore, Style
    init()
@@ -75,24 +74,73 @@ except:
 reader = csv.reader(sys.stdin)
 maxid = 0
 for row in reader:
-   if row[4] in [ "userlist", "eventid" ]:
+   if row[4] == "userlist":
+      if row[6] != "0":
+         print(Fore.CYAN + "Userlist: " + (row[2] or "EMPTY") + " " + 
+            Style.DIM + Fore.MAGENTA + row[3] + Style.RESET_ALL)
+      continue 
+   elif row[4] == "eventId":
       continue
-   print(Style.BRIGHT + Fore.GREEN + row[1] + "  " + Style.DIM + Fore.MAGENTA + row[3])
-   print(Style.RESET_ALL + Fore.WHITE + row[2])
+   if row[1] == os.environ["capi_username"]:
+      uc = Fore.CYAN
+   else:
+      uc = Fore.GREEN
+   print(Style.BRIGHT + uc + "[" + row[1] + "] ", end="")
+   if "E" in row[5]:
+      print(Style.RESET_ALL + Fore.RED + row[8] + "(edit)", end="")
+   else:
+      print(Style.DIM + Fore.WHITE + row[8], end="")
+   print(" " + Style.DIM + Fore.MAGENTA + row[3])
+   lines = textwrap.fill(row[2].rstrip("\n"),
+      width=shutil.get_terminal_size().columns - 2,
+      replace_whitespace=False).split("\n")
+   print(Style.RESET_ALL,end="")
+   for line in lines:
+      print(" " + line)
    if row[8]:
       thisid = int(row[8])
       if thisid > maxid:
          maxid = thisid
 print(maxid, file=sys.stderr)
+if maxid == 0:
+   exit(55)
 '
+
+# ALL listens are mostly the same, luckily.
+capi_listen() {
+   while true
+   do
+      newmid=$(curl -s -H "Authorization: Bearer $CAPI_TOKEN" -G \
+         --data-urlencode "get=$1" \
+         --data-urlencode "rooms=$room" \
+         --data-urlencode "mid=$mid" \
+         "$CAPI_URL/small/chat" | python3 -c "$pymessage" 2>&1 >/dev/tty)
+
+      err=$?
+
+      if [ "$err" != "0" ]
+      then
+         >&2 echo "Listen error; waiting 10 seconds"
+         sleep 10
+      else
+         break
+      fi
+   done
+
+   if [ $newmid -gt $mid ]
+   then
+      mid=$newmid
+   fi
+}
 
 # The initial call pulls some amount, the later calls are an infinite loop.
 # If the call fails, we wait for some time before trying again...
 
-curl -s -H "Authorization: Bearer $CAPI_TOKEN" -G \
-   --data-urlencode "get=-$pull" \
-   --data-urlencode "rooms=$room" \
-   --data-urlencode "mid=$mid" \
-   "$CAPI_URL/small/chat" | python3 -c "$pymessage"
+capi_listen "-$pull"
 
-echo "DONE"
+while true
+do
+   capi_listen "$pull"
+done
+
+echo "DONE: $mid"
