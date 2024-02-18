10ea5866d67452c1f825e224d2fbc25f291d78c6
Carlos Sanchez <randomouscrap98@aol.com>
2024-02-17 19:25:19 -0500

    search done?

---
+++ python/search.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import io
+import os
+import sys
+import csv
+import getpass
+import urllib.parse
+import urllib.request
+from urllib.error import HTTPError
+
+# User input setup
+capiurl=os.environ.get("CAPI_URL")
+capitoken=os.environ.get("CAPI_TOKEN")
+
+def perror(message, end=os.linesep):
+    print(message, end=end, file=sys.stderr)
+    sys.stderr.flush()
+
+if not capiurl:
+    perror("Error: CAPI_URL environment variable not set")
+    exit(1)
+
+params = {}
+perror("Search: ", end="")
+params["search"] = "%" + input() + "%"
+
+# Set up request
+encoded_params = urllib.parse.urlencode(params)
+url = capiurl + "/small/search?" + encoded_params
+req = urllib.request.Request(url)
+
+if capitoken:
+    perror("Using user token")
+    req.add_header("Authorization", "Bearer " + capitoken)
+
+response = urllib.request.urlopen(req)
+reader = csv.reader(io.StringIO(response.read().decode('utf-8')))
+
+# Print results
+for row in reader:
+   if "R" not in row[5]:
+      private = "P"
+   else:
+      private = " "
+   print("{}{:>7} - {}".format(private, row[6], row[0]))
+
