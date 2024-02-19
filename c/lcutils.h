27b23d9c48a377087fab4e62c1eae707fd267d3a
Carlos Sanchez <randomouscrap98@aol.com>
2024-02-19 02:45:45 -0500

    chatgpt colors

--- c/lcutils.h
+++ c/lcutils.h
@@ -1,6 +1,49 @@
 #ifndef LC_LCUTILS_GUARD
 #define LC_LCUTILS_GUARD
 
+#ifdef BUILDWINDOWS
+/* Windows does not support ANSI escape codes, so define empty macros */
+#define CFORE_RESET   ""
+#define CFORE_BLACK   ""
+#define CFORE_RED     ""
+#define CFORE_GREEN   ""
+#define CFORE_YELLOW  ""
+#define CFORE_BLUE    ""
+#define CFORE_MAGENTA ""
+#define CFORE_CYAN    ""
+#define CFORE_WHITE   ""
+
+#define CFORE_B_BLACK   ""
+#define CFORE_B_RED     ""
+#define CFORE_B_GREEN   ""
+#define CFORE_B_YELLOW  ""
+#define CFORE_B_BLUE    ""
+#define CFORE_B_MAGENTA ""
+#define CFORE_B_CYAN    ""
+#define CFORE_B_WHITE   ""
+#else
+/* ANSI escape codes for colors */
+#define CFORE_RESET   "\x1b[0m"
+#define CFORE_BLACK   "\x1b[30m"
+#define CFORE_RED     "\x1b[31m"
+#define CFORE_GREEN   "\x1b[32m"
+#define CFORE_YELLOW  "\x1b[33m"
+#define CFORE_BLUE    "\x1b[34m"
+#define CFORE_MAGENTA "\x1b[35m"
+#define CFORE_CYAN    "\x1b[36m"
+#define CFORE_WHITE   "\x1b[37m"
+
+/* Bright versions of ANSI colors */
+#define CFORE_B_BLACK   "\x1b[90m"
+#define CFORE_B_RED     "\x1b[91m"
+#define CFORE_B_GREEN   "\x1b[92m"
+#define CFORE_B_YELLOW  "\x1b[93m"
+#define CFORE_B_BLUE    "\x1b[94m"
+#define CFORE_B_MAGENTA "\x1b[95m"
+#define CFORE_B_CYAN    "\x1b[96m"
+#define CFORE_B_WHITE   "\x1b[97m"
+#endif
+
 void error(char * fmt, ...);
 void lc_makesearch(char * string, size_t maxlen);
 
